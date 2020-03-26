c4c20cfad09cea6c228df19c638b7e52972ccf12
dlbunker <mcirque@gmail.com>
2020-03-25 22:57:35 -0600

    initial app checkin

---
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conferencedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceDemoApplication.class, args);
+	}
+
+}
---
+++ conference-demo/src/test/java/com/pluralsight/conferencedemo/ConferenceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conferencedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
