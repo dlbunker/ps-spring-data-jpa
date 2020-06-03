b706cce6753c7b1b017b0450dd22f21dff9c65de
dlbunker <mcirque@gmail.com>
2020-06-02 22:53:32 -0600

    session repo proxy to jpa repo

---
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionJpaRepository.java
@@ -0,0 +1,7 @@
+package com.pluralsight.conferencedemo.repositories;
+
+import com.pluralsight.conferencedemo.models.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessionJpaRepository extends JpaRepository<Session, Long> {
+}
--- conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionRepository.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionRepository.java
@@ -1,6 +1,7 @@
 package com.pluralsight.conferencedemo.repositories;
 
 import com.pluralsight.conferencedemo.models.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -9,32 +10,30 @@ import java.util.List;
 
 @Repository
 public class SessionRepository {
+    @Autowired
+    private SessionJpaRepository jpaRepository;
+
     @PersistenceContext
     private EntityManager entityManager;
 
     public Session create(Session session) {
-        entityManager.persist(session);
-        entityManager.flush();
-        return session;
+        return jpaRepository.saveAndFlush(session);
     }
 
     public Session update(Session session) {
-        session = entityManager.merge(session);
-        entityManager.flush();
-        return session;
+        return jpaRepository.saveAndFlush(session);
     }
 
     public void delete(Long id) {
-        entityManager.remove(find(id));
-        entityManager.flush();
+        jpaRepository.deleteById(id);
     }
 
     public Session find(Long id) {
-        return entityManager.find(Session.class, id);
+        return jpaRepository.getOne(id);
     }
 
     public List<Session> list() {
-        return entityManager.createQuery("select s from sessions s").getResultList();
+        return jpaRepository.findAll();
     }
 
     public List<Session> getSessionsThatHaveName(String name) {
