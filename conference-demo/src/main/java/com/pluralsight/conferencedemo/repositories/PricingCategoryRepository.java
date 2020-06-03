f6ea0c3b0aa55382748d12610ea0d0512168b02d
dlbunker <mcirque@gmail.com>
2020-06-02 22:15:58 -0600

    ticket type test support

---
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/PricingCategoryRepository.java
@@ -0,0 +1,17 @@
+package com.pluralsight.conferencedemo.repositories;
+
+import com.pluralsight.conferencedemo.models.PricingCategory;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class PricingCategoryRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public PricingCategory find(String id) {
+        return entityManager.find(PricingCategory.class, id);
+    }
+}
