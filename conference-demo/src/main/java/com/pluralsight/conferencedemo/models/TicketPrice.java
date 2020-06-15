29f284a170e44da5e998a9b75793ddf7e6cece36
dlbunker <mcirque@gmail.com>
2020-06-14 22:15:52 -0600

    Update TicketPrice.java

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/models/TicketPrice.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/models/TicketPrice.java
@@ -7,6 +7,17 @@ import java.math.BigDecimal;
 
 @Entity
 @Table(name = "ticket_prices")
+@NamedQuery(
+        name = "TicketPrice.namedFindTicketsByPricingCategoryName",
+        query = "select tp from TicketPrice tp where tp.pricingCategory.pricingCategoryName = :name"
+)
+@NamedNativeQuery(
+        name = "TicketPrice.nativeFindTicketsByCategoryWithWorkshop",
+        query = "select * from ticket_prices tp " +
+                "left join ticket_types tt on tp.ticket_type_code = tt.ticket_type_code " +
+                "left join pricing_categories pc on tp.pricing_category_code = pc.pricing_category_code " +
+                "where tt.includes_workshop = true and pc.pricing_category_name = :name"
+)
 public class TicketPrice {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
