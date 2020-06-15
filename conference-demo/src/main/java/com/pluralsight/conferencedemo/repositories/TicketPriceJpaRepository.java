a3a84d43c804130c73409d810ca3f4e7b4845cae
dlbunker <mcirque@gmail.com>
2020-06-14 22:58:21 -0600

    named queries

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/TicketPriceJpaRepository.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/TicketPriceJpaRepository.java
@@ -3,6 +3,7 @@ package com.pluralsight.conferencedemo.repositories;
 import com.pluralsight.conferencedemo.models.TicketPrice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -12,4 +13,7 @@ public interface TicketPriceJpaRepository extends JpaRepository<TicketPrice, Lon
             "and tp.ticketType.includesWorkshop = true")
     List<TicketPrice> getTicketsUnderPriceWithWorkshops(BigDecimal maxPrice);
 
+    List<TicketPrice> namedFindTicketsByPricingCategoryName(@Param("name") String name);
+
+    List<TicketPrice> nativeFindTicketsByCategoryWithWorkshop(@Param("name") String name);
 }
--- conference-demo/src/test/java/com/pluralsight/conferencedemo/models/TicketPriceTest.java
+++ conference-demo/src/test/java/com/pluralsight/conferencedemo/models/TicketPriceTest.java
@@ -64,4 +64,15 @@ public class TicketPriceTest {
         assertTrue(tickets.size() > 0);
     }
 
+    @Test
+    public void testNamedQuery() throws Exception {
+        List<TicketPrice> tickets = repository.namedFindTicketsByPricingCategoryName("Regular");
+        assertTrue(tickets.size() > 0);
+    }
+
+    @Test
+    public void testNamedNativeQuery() throws Exception {
+        List<TicketPrice> tickets = repository.nativeFindTicketsByCategoryWithWorkshop("Regular");
+        assertTrue(tickets.size() > 0);
+    }
 }
