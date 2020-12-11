8813ab50f56bc777aa9f8107be2e5bd4ef8fdad3
Летов Никита Кириллович <letnik86@gmail.com>
2020-12-11 19:47:41 +0300

    Comment for feature-1.0

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConferenceDemoApplication {
 
+	//NewFeature-1.0
 	public static void main(String[] args) {
 		SpringApplication.run(ConferenceDemoApplication.class, args);
 	}
